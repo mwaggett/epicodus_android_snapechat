2483261cb3b2a044f9314407e6677ed5a4dd4183
mwaggett <mwaggett@alumni.reed.edu>
2015-10-23 15:17:33 -0700

    Add SnapeLib and add buttons to view different pictures of Snape.

--- app/src/main/java/io/github/mwaggett/snapechat/ShowPhotosActivity.java
+++ app/src/main/java/io/github/mwaggett/snapechat/ShowPhotosActivity.java
@@ -1,14 +1,62 @@
 package io.github.mwaggett.snapechat;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ShowPhotosActivity extends AppCompatActivity {
+
+    private ImageView mImage;
+    private Button mNextButton;
+    private Button mPreviousButton;
+    private Button mRandomButton;
+    private SnapeLib mSnapeLib;
+    private int mCurrentSnape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_photos);
+
+        mImage = (ImageView) findViewById(R.id.currentSnape);
+        mNextButton = (Button) findViewById(R.id.nextButton);
+        mPreviousButton = (Button) findViewById(R.id.previousButton);
+        mRandomButton = (Button) findViewById(R.id.randomButton);
+        mSnapeLib = new SnapeLib();
+        mCurrentSnape = mSnapeLib.randomSnape();
+
+        setLayoutContent();
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentSnape = mSnapeLib.nextSnape(mCurrentSnape);
+                setLayoutContent();
+            }
+        });
+
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentSnape = mSnapeLib.previousSnape(mCurrentSnape);
+                setLayoutContent();
+            }
+        });
+
+        mRandomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentSnape = mSnapeLib.randomSnape();
+                setLayoutContent();
+            }
+        });
+    }
+
+    private void setLayoutContent() {
+        mImage.setImageResource(mCurrentSnape);
     }
 
 }
