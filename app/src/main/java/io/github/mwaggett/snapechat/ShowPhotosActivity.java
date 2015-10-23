1a6393b67853db0df6e6022f51a82badc3780ee7
mwaggett <mwaggett@alumni.reed.edu>
2015-10-23 13:25:47 -0700

    Create banner. Buttons designed but hidden for the moment.

---
+++ app/src/main/java/io/github/mwaggett/snapechat/ShowPhotosActivity.java
@@ -0,0 +1,14 @@
+package io.github.mwaggett.snapechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowPhotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_photos);
+    }
+
+}
