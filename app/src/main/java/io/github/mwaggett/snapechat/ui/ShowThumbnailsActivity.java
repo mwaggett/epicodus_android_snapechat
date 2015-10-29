4b7b496c9e7c6ebc3b262bdad61b2362a4d11059
mwaggett <mwaggett@alumni.reed.edu>
2015-10-29 11:35:13 -0700

    In progress: experimenting with grid view.

---
+++ app/src/main/java/io/github/mwaggett/snapechat/ui/ShowThumbnailsActivity.java
@@ -0,0 +1,31 @@
+package io.github.mwaggett.snapechat.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import io.github.mwaggett.snapechat.R;
+
+public class ShowThumbnailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_thumbnails);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(ShowThumbnailsActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
